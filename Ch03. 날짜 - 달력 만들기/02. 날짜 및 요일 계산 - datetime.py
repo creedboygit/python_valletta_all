6b2aa07eeca3c47d862afe833aaf6730b28a0229
Ju Hyun <polozh@naver.com>
2024-01-31 23:22:49 +0900

    날짜

--- Ch03. 날짜 - 달력 만들기/02. 날짜 및 요일 계산 - datetime.py
+++ Ch03. 날짜 - 달력 만들기/02. 날짜 및 요일 계산 - datetime.py
@@ -19,3 +19,35 @@ day = datetime.datetime(2024, 2, 12)
 time = datetime.time(20, 32, 11)
 dt = datetime.datetime.combine(day, time)
 print(dt)
+
+# 현재 날짜/시간 - today, now
+today = datetime.date.today()
+now = datetime.datetime.now()
+
+print("오늘: %s" % today)
+print("지금: %s" % now)
+
+# print("함수 수행시간 : %f 초" % (end - start))
+
+# 날짜 뺄셈
+day1 = datetime.date(2023, 12, 10)
+day2 = datetime.date(2024, 1, 20)
+
+diff = day2 - day1
+print(diff)
+
+# 날짜 뺄셈
+day1 = datetime.datetime(2023, 12, 10, 15, 2, 3)
+day2 = datetime.datetime(2024, 1, 20, 20, 30, 20)
+
+diff = day2 - day1
+print(diff)
+
+# 날짜 덧셈
+plus = datetime.timedelta(weeks=10, days=10, hours=1, minutes=10, seconds=10)
+print(plus)
+
+add = day1 + plus
+print(add)
+
+
