502ee5580d48ddc367373ec3f8165eb20213f212
creedboy <polozh@naver.com>
2024-02-17 15:24:09 +0900

    병렬처리 웹 크롤러

---
+++ Ch07. [시스템] N배 빠른 병렬처리 웹 크롤러 만들기/03. 매개변수 01.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == "__main__":
+    print(sys.argv)
+
+    val1 = int(sys.argv[1])
+    val2 = int(sys.argv[2])
+
+    print(val1, ' °öÇÏ±â ', val2, ' Àº? ', (val1 * val2))
