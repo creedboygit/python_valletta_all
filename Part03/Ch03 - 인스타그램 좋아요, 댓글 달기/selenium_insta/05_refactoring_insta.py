5434c918d56ea894955f1d17eeeff29ae20b9934
한샘몰백엔드개발팀 <polozh@naver.com>
2024-02-23 21:38:21 +0900

    인스타 크롤링

--- Part03/Ch03 - 인스타그램 좋아요, 댓글 달기/selenium_insta/05_refactoring_insta.py
+++ Part03/Ch03 - 인스타그램 좋아요, 댓글 달기/selenium_insta/05_refactoring_insta.py
@@ -1,9 +1,8 @@
-from selenium import webdriver
-import chromedriver_autoinstaller
-import time
-import getpass
 import os
+import time
 
+import chromedriver_autoinstaller
+from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
@@ -24,43 +23,52 @@ driver.get(url=url)
 
 # //*[@id="loginForm"]/div/div[1]/div/label/input
 
+time.sleep(2)
+
+
+def login(id, pw):
+    xpath_id = '//*[@id="loginForm"]/div/div[1]/div/label/input'
+    xpath_pw = '//*[@id="loginForm"]/div/div[2]/div/label/input'
+
+    input_id = driver.find_element(By.XPATH, xpath_id)
+    input_id.send_keys(id)
+
+    input_pw = driver.find_element(By.XPATH, xpath_pw)
+    input_pw.send_keys(pw)
+
+    time.sleep(2)
+
+    # driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]').click()
+    driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button').send_keys(Keys.ENTER)
+
+
+def search(hashtag, scroll_times):
+    # url = "https://www.instagram.com/explore/tags/고양이"
+    url = f"https://www.instagram.com/explore/tags/{hashtag}"
+    driver.get(url=url)
+
+    time.sleep(5)
+    print("======= driver.page_source: " + str(driver.page_source))
+
+    # Scroll
+    # for _ in range(5):
+    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #     time.sleep(7)
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+
 # Login
 id = os.getenv("INSTA_ID")
 # pw = getpass.getpass("비밀번호를 입력해주세요. >>> ")
 pw = os.getenv("INSTA_PW")
 
-time.sleep(2)
-
-xpath_id = '//*[@id="loginForm"]/div/div[1]/div/label/input'
-xpath_pw = '//*[@id="loginForm"]/div/div[2]/div/label/input'
-
-input_id = driver.find_element(By.XPATH, xpath_id)
-input_id.send_keys(id)
-
-input_pw = driver.find_element(By.XPATH, xpath_pw)
-input_pw.send_keys(pw)
-
-time.sleep(2)
-
-# driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]').click()
-driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button').send_keys(Keys.ENTER)
+login(id, pw)
 
 time.sleep(4)
 
 # Hashtag Search
 hashtag = "고양이"
-# url = "https://www.instagram.com/explore/tags/고양이"
-url = f"https://www.instagram.com/explore/tags/{hashtag}"
-driver.get(url=url)
-
-time.sleep(5)
-print("======= driver.page_source: " + str(driver.page_source))
-
-# Scroll
-# for _ in range(5):
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#     time.sleep(7)
-    
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+search(hashtag, 1)
 
 time.sleep(20)
