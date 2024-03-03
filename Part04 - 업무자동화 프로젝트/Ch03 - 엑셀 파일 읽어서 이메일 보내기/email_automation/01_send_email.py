06f11ca2f1a1fdbd9bdef7952a71d8407711700c
한샘몰백엔드개발팀 <polozh@naver.com>
2024-03-03 19:35:37 +0900

    이메일 보내기 자동화

--- Part04 - 업무자동화 프로젝트/Ch03 - 엑셀 파일 읽어서 이메일 보내기/email_automation/01_send_email.py
+++ Part04 - 업무자동화 프로젝트/Ch03 - 엑셀 파일 읽어서 이메일 보내기/email_automation/01_send_email.py
@@ -1,5 +1,6 @@
 import os
 import smtplib
+from email.mime.text import MIMEText
 
 
 class EmailSender:
@@ -18,14 +19,26 @@ class EmailSender:
         :param to_addr: 받는 사람
         """
 
-        with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+        # with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+        with smtplib.SMTP("smtp.naver.com", 587) as smtp:
+            msg = MIMEText(msg)
+            msg['From'] = from_addr
+            msg['To'] = to_addr
+            msg['Subject'] = "메일 발송 테스트"
+            print(msg.as_string())
+
             smtp.starttls()
-            smtp.login(self.email_addr, self.password)
-            smtp.sendmail(from_addr=from_addr, to_addrs=to_addr, msg=msg.encode("utf-8"))
+            # smtp.login(self.email_addr, self.password)
+            smtp.login("polozh@naver.com", self.password)
+            # smtp.sendmail(from_addr=from_addr, to_addrs=to_addr, msg=msg.encode("utf-8"))
+            smtp.sendmail(from_addr=from_addr, to_addrs=to_addr, msg=msg.as_string())
             smtp.quit()
         print("이메일 전송이 완료되었습니다.")
 
 
 if __name__ == '__main__':
-    es = EmailSender("polozhzh@gmail.com", os.getenv("MY_GMAIL_PASSWORD"))
-    es.send_email("테스트입니다 2", from_addr="polozhzh@gmail.com", to_addr="polozh@naver.com")
+    # es = EmailSender("polozhzh@gmail.com", os.getenv("MY_GMAIL_PASSWORD"))
+    # es.send_email("테스트입니다 2", from_addr="polozhzh@gmail.com", to_addr="polozh@naver.com")
+    es = EmailSender("polozh@naver.com", os.getenv("MY_NAVER_PASSWORD"))
+    # es.send_email("테스트입니다 2", from_addr="polozh@naver.com", to_addr="polozhzh@gmail.com")
+    es.send_email("테스트입니다 2", from_addr="polozh@naver.com", to_addr="polozhzh@gmail.com")
